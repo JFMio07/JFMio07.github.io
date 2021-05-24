dc9c3bc83b68cceceb109052228138a8c949cf77
JF <tksymio@gmail.com>
2021-05-24 18:30:44 +0800

    editing scroll-menu-list js code

--- ProjectEva/categories/js/categories-2.js
+++ ProjectEva/categories/js/categories-2.js
@@ -24,9 +24,8 @@ window.onload = function () {
 
         }
     })
- 
+
     let scrollMenuObj = new scrollMenu("scroll-menu", 280, 7);
-    // let scrollMenuObj = new scrollMenu("scroll-menu", 280, 3);
     scrollMenuObj.Initialize();
     // console.log(scrollMenuObj.element);
 
@@ -34,20 +33,146 @@ window.onload = function () {
 }
 
 class scrollMenu {
-    constructor(IDName, pageHeight, itemOfPage) {
-        this.IDName = IDName;
-        this.pageHeight = pageHeight;
-        this.itemOfPage = itemOfPage;
+    constructor(IDName, pageHeight, itemsPerPage) {
+        let parentIDName = IDName;
         let parentDOM = null;
+        let currentPOS = 0;
+        this.pageHeight = pageHeight;
+        this.itemsPerPage = itemsPerPage;
 
+
+        // Initialize parts of scrollMenu
         this.Initialize = function () {
-            parentDOM = document.getElementById(this.IDName);
+            parentDOM = document.getElementById(parentIDName);
             parentDOM.querySelector(".menu-list-wrap").style.height = this.pageHeight + "px";
-            parentDOM.querySelectorAll(".menu-list-item").forEach(elm => elm.style.height = (this.pageHeight / this.itemOfPage) + "px");
+            parentDOM.querySelectorAll(".menu-list-item").forEach(elm => elm.style.height = (this.pageHeight / this.itemsPerPage) + "px");
+            // parentDOM.querySelector(".scroll-previous").setAttribute("disabled", "");
+            parentDOM.querySelector(".scroll-previous").addEventListener("click", () => this.ScrollingMenu(true));
+            // parentDOM.querySelector(".scroll-next").removeAttribute("disabled");
+            parentDOM.querySelector(".scroll-next").addEventListener("click", () => this.ScrollingMenu(false));
+        }
 
-            // console.log(parentDOM.querySelector(".menu-list-wrap"));
-            // console.log(parentDOM);
+        this.ScrollingMenu = async function (scrollDirection) {
+            let destposition = 0;
+            let outOfRange = false;
+            let menuHeight = parentDOM.querySelector(".menu-list-item-wrap").getBoundingClientRect().height;
+            console.log(menuHeight);
+            if (scrollDirection) {
+                destposition = currentPOS + this.pageHeight;
+            } else {
+                destposition = currentPOS - this.pageHeight;
+            }
+
+            if (destposition > 0 ||) {
+                return
+            }
+            currentPOS = destposition;
+            let result = await this.ScrollElementByPositionY(".menu-list-item-wrap", destposition);
+            console.log(result);
+
+            console.log(scrollDirection);
         }
     }
 
+    //
+    ScrollElementByPositionY(menuClassName, destPosition, frame = 20, millSecondPerFrame = 15) {
+        let elem = document.querySelector(menuClassName);
+
+        if (elem == null || destPosition == null) { throw new Error("input parameter error."); }
+
+        elem.style.top = elem.style.top === "" ? "0px" : elem.style.top;
+        let startPosition = parseFloat(elem.style.top);
+        frame = frame > 0 ? frame : 1;
+
+        //scroll direction of Y-axis
+        //false : scroll down ;
+        //true : scroll up ; 
+        let scrollDirection = destPosition > startPosition;
+        let perScroll = (Math.abs(destPosition - startPosition) / frame);
+
+        return new Promise((resolve, reject) => {
+            if (perScroll !== 0) {
+                for (let i = 0; i < frame; i++) {
+                    ((t) => {
+                        setTimeout(() => {
+                            let currentPos = parseFloat(elem.style.top);
+                            let currentdiff = Math.abs((destPosition - currentPos));
+                            let newPerScroll = currentdiff >= perScroll ? perScroll : currentdiff;
+                            newPerScroll *= (scrollDirection ? 1 : -1);
+
+                            elem.style.top = currentPos + newPerScroll + "px";
+
+                            // console.log(parseFloat(elem.style.top));
+
+                            if (parseFloat(elem.style.top) == destPosition) {
+                                resolve("Scroll Completed");
+                            }
+                        }, t);
+                    })(millSecondPerFrame * i)
+                }
+            }
+            else {
+                resolve("Scroll Completed");
+            }
+        })
+    }
+
 }
+
+// function scrollMenu(IDName, pageHeight, itemOfPage) {
+//     this.IDName = IDName;
+//     this.pageHeight = pageHeight;
+//     this.itemOfPage = itemOfPage;
+//     let parentDOM = null;
+
+//     this.Initialize = function () {
+//         parentDOM = document.getElementById(this.IDName);
+//         parentDOM.querySelector(".menu-list-wrap").style.height = this.pageHeight + "px";
+//         parentDOM.querySelectorAll(".menu-list-item").forEach(elm => elm.style.height = (this.pageHeight / this.itemOfPage) + "px");
+//         parentDOM.querySelector(".scroll-previous").setAttribute("disabled","");
+//         parentDOM.querySelector(".scroll-next").removeAttribute("disabled");
+//     }
+// }
+
+
+// function ScrollElementByPositionY(elementId, destPosition, frame = 20, millSecondPerFrame = 15) {
+//     let elem = document.getElementById(elementId);
+
+//     if (elem == null || destPosition == null) { throw new Error("input parameter error."); }
+
+//     elem.style.top = elem.style.top === "" ? "0px" : elem.style.top;
+//     let startPosition = parseFloat(elem.style.top);
+//     frame = frame > 0 ? frame : 1;
+
+//     //scroll direction of Y-axis
+//     //false : scroll down ;
+//     //true : scroll up ; 
+//     let scrollDirection = destPosition > startPosition;
+//     let perScroll = (Math.abs(destPosition - startPosition) / frame);
+
+//     return new Promise((resolve, reject) => {
+//         if (perScroll !== 0) {
+//             for (let i = 0; i < frame; i++) {
+//                 ((t) => {
+//                     setTimeout(() => {
+//                         let currentPos = parseFloat(elem.style.top);
+//                         let currentdiff = Math.abs((destPosition - currentPos));
+//                         let newPerScroll = currentdiff >= perScroll ? perScroll : currentdiff;
+//                         newPerScroll *= (scrollDirection ? 1 : -1);
+
+//                         elem.style.top = currentPos + newPerScroll + "px";
+
+//                         // console.log(parseFloat(elem.style.top));
+
+//                         if (parseFloat(elem.style.top) == destPosition) {
+//                             resolve("Scroll Completed");
+//                         }
+//                     }, t);
+//                 })(millSecondPerFrame * i)
+//             }
+//         }
+//         else {
+//             resolve("Scroll Completed");
+//         }
+//     })
+// }
