7243e1812bb4c7f80524b7477665d50357fb6b5f
JF <tksymio@gmail.com>
2021-07-20 18:34:03 +0800

    create 1st version NonSale Page

---
+++ OHwOwAdmin-template/Assets/js/helpers.js
@@ -0,0 +1,21 @@
+//價錢三位一點
+function CurrencyFormat(num) {
+    var parts = num.toString().split('.');
+    parts[0] = parts[0].replace(/\B(?=(\d{3})+(?!\d))/g, ',');
+    return parts.join('.');
+}
+
+//日期時間格式化
+function DateFormat(value, format) {
+    return moment(value).format(format || 'YYYY-MM-DD');
+}
+
+//超過指定內容長度以...取代
+function StringContentFormat(content, maxlength = 1) {
+    if (maxlength <= 0 || content.length <= maxlength) {
+        return content;
+    }
+    else {
+        return content.substring(0, maxlength - 1) + '...';
+    }
+}
--- OHwOwAdmin-template/Assets/js/productlist.js
+++ OHwOwAdmin-template/Assets/js/productlist.js
@@ -3,54 +3,71 @@ let app = new Vue({
     data() {
         return {
             isBusy: false,
-            items: [
-                { isActive: true, age: 40, name: { first: 'Dickerson', last: 'Macdonald' } },
-                { isActive: false, age: 21, name: { first: 'Larsen', last: 'Shaw' } },
-                {
-                    isActive: false,
-                    age: 9,
-                    name: { first: 'Mini', last: 'Navarro' },
-                    _rowVariant: 'success'
-                },
-                { isActive: false, age: 89, name: { first: 'Geneva', last: 'Wilson' } },
-                { isActive: true, age: 38, name: { first: 'Jami', last: 'Carney' } },
-                { isActive: false, age: 27, name: { first: 'Essie', last: 'Dunlap' } },
-                { isActive: true, age: 40, name: { first: 'Thor', last: 'Macdonald' } },
-                {
-                    isActive: true,
-                    age: 87,
-                    name: { first: 'Larsen', last: 'Shaw' },
-                    _cellVariants: { age: 'danger', isActive: 'warning' }
-                },
-                { isActive: false, age: 26, name: { first: 'Mitzi', last: 'Navarro' } },
-                { isActive: false, age: 22, name: { first: 'Genevieve', last: 'Wilson' } },
-                { isActive: true, age: 38, name: { first: 'John', last: 'Carney' } },
-                { isActive: false, age: 29, name: { first: 'Dick', last: 'Dunlap' } }
-            ],
+            onSaleItems: [],
+            nonSaleItems: [],
             fields: [
                 { key: 'productId', label: '商品編號', sortable: true, sortDirection: 'desc' },
-                { key: 'productName', label: '商品名稱', sortable: true, sortDirection: 'desc' },
-                { key: 'productImg', label: '商品圖片' },
-                { key: 'authorName', label: '作者', sortable: true, sortDirection: 'desc' },
-                { key: 'publisherName', label: '出版社', sortable: true, sortDirection: 'desc' },
-                { key: 'mainCategory', label: '主分類', sortable: true, sortDirection: 'desc' },
-                { key: 'subCategory', label: '子分類', sortable: true, sortDirection: 'desc' },
-                { key: 'fixedPrice', label: '單價', sortable: true, sortDirection: 'desc' },
-                { key: 'name', label: 'Person full name', sortable: true, sortDirection: 'desc' },
-                { key: 'age', label: 'Person age', sortable: true, class: 'text-center' },
                 {
-                    key: 'isActive',
-                    label: 'Is Active',
+                    key: 'productName',
+                    label: '商品名稱',
                     formatter: (value, key, item) => {
-                        return value ? 'Yes' : 'No'
+                        return StringContentFormat(value, 10);
+                    },
+                    sortable: false,
+                    sortDirection: 'desc'
+                },
+                { key: 'productImg', label: '商品圖片' },
+                {
+                    key: 'authorName',
+                    label: '作者',
+                    formatter: (value, key, item) => {
+                        return StringContentFormat(value, 10);
+                    },
+                    sortable: false,
+                    sortDirection: 'desc'
+                },
+                {
+                    key: 'publisherName',
+                    label: '出版社',
+                    formatter: (value, key, item) => {
+                        return StringContentFormat(value, 10);
+                    },
+                    sortable: true,
+                    sortDirection: 'desc'
+                },
+                {
+                    key: 'mainCategory',
+                    label: '主分類',
+                    formatter: (value, key, item) => {
+                        return StringContentFormat(value, 10);
+                    },
+                    sortable: true,
+                    sortDirection: 'desc'
+                },
+                {
+                    key: 'fixedPrice',
+                    label: '單價',
+                    formatter: (value, key, item) => {
+                        return 'NT$' + CurrencyFormat(value);
+                    },
+                    sortable: true,
+                    sortDirection: 'desc'
+                },
+                {
+                    key: 'shelfDate',
+                    label: '上架日期',
+                    formatter: (value, key, item) => {
+                        if (value == null) { return "" }
+                        return DateFormat(value)
+                        // return moment(value).format('YYYY-MM-DD HH:mm:ss');
                     },
                     sortable: true,
                     sortByFormatted: true,
                     filterByFormatted: true
                 },
-                { key: 'actions', label: 'Actions' }
+                { key: 'actions', label: '商品管理' }
             ],
-            totalRows: 1,
+            totalOnSaleRows: 1,
             currentPage: 1,
             perPage: 5,
             pageOptions: [5, 10, 15],
@@ -63,6 +80,10 @@ let app = new Vue({
                 id: 'info-modal',
                 title: '',
                 content: ''
+            },
+            urllist: {
+                simplifyproductsOnSale: 'https://localhost:5001/api/Product/GetSimplifyProductsOnSale',
+                simplifyproductsNonSale: 'https://localhost:5001/api/Product/GetSimplifyProductsNonSale',
             }
         }
     },
@@ -76,34 +97,51 @@ let app = new Vue({
                 })
         }
     },
+    watch: {
+        onSaleItems: function () {
+            this.totalOnSaleRows = this.onSaleItems.length
+        },
+    },
     created() {
-        this.getSimplifyProducts();
+        this.getSimplifyProductssOnSale(this.urllist.simplifyproductsOnSale);
     },
     mounted() {
-        // Set the initial number of items
-        this.totalRows = this.items.length
+       
     },
     methods: {
-        getSimplifyProducts() {
-            this.isBusy = true;
+        getSimplifyProductssOnSale(uri) {
+            // console.log(uri);
             let cfg = {
                 methods: 'get',
                 headers: { 'Content-type': 'application/json' },
-                url: 'https://localhost:5001/api/Product/GetSimplifyProducts'
+                url: uri
             };
 
             axios(cfg)
                 .then(res => {
                     console.log(res);
-                    console.log(res.data);
-                    console.log(res.status);
                     if (Array.isArray(res.data.result) && res.status == 200) {
                         this.isBusy = false;
+                        this.onSaleItems = res.data.result.map((item, index) => {
+                            return {
+                                productId: item.productID,
+                                productName: item.productName,
+                                // productImg: item.imagePath,
+                                productImg: './Assets/image/book-sm-pic.jpg',
+                                authorName: item.authorName,
+                                publisherName: item.publisherName,
+                                mainCategory: item.mainCategoryName,
+                                subCategory: item.subCategoryName,
+                                fixedPrice: item.fixedPrice,
+                                shelfDate: item.shelfDate
+                            }
+                        });
                     }
                 })
                 .catch(err => {
                     console.log(err);
                 });
+            this.isBusy = true;
         },
         info(item, index, button) {
             this.infoModal.title = `Row index: ${index}`
@@ -116,7 +154,7 @@ let app = new Vue({
         },
         onFiltered(filteredItems) {
             // Trigger pagination to update the number of buttons/pages due to filtering
-            this.totalRows = filteredItems.length
+            this.totalOnSaleRows = filteredItems.length
             this.currentPage = 1
         },
         toggleBusy() {
