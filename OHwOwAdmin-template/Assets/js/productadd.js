59573e87d0069d38dc6f46f404e7a464820f5140
JF <tksymio@gmail.com>
2021-07-26 00:47:50 +0800

    加入BootstrapVue file input

--- OHwOwAdmin-template/Assets/js/productadd.js
+++ OHwOwAdmin-template/Assets/js/productadd.js
@@ -14,6 +14,7 @@ let app = new Vue({
                 publisher:'',
                 mainCategory:'',
                 subCategory:'',
+                file: null,
             },
             
             inputDataCheck: {
